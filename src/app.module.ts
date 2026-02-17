608b7faa07f2fdb1d698faecc0adfce3bc05e630
masud2005 <masud20012005@gmail.com>
2026-02-17 23:21:13 +0600

    modules template

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { PrismaModule } from './prisma/prisma.module';
       envFilePath: '.env',
     }),
     PrismaModule,
+    ModulesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
