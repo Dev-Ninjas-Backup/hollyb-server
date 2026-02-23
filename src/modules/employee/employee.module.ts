3fb499fc4e4799a94487db4d823e864f9420ff60
masud2005 <masud20012005@gmail.com>
2026-02-24 00:26:20 +0600

    latest jobs, all jobs api and details api add

--- src/modules/employee/employee.module.ts
+++ src/modules/employee/employee.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
+import { PrismaModule } from '@/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [EmployeeService],
   controllers: [EmployeeController],
 })
