f50af3b70414fd5283d9c59502f622c564564447
Rafi-Sharka <rafisharkar144@gmail.com>
2026-03-04 03:37:42 +0600

    Sanitize the the format

--- src/modules/admin/settings/settings.service.ts
+++ src/modules/admin/settings/settings.service.ts
@@ -83,6 +83,11 @@ export class SettingsService {
         ...dto,
         updated_by: userId,
       },
+      select: {
+        id: true,
+        workspaceName: true,
+        Timezone: true
+      }
     });
 
     return updatedSettings;
@@ -110,6 +115,12 @@ export class SettingsService {
         ...dto,
         updated_by: userId,
       },
+      select: {
+        id: true,
+        two_factor_authentication_enabled: true,
+        system_alerts_enabled: true,
+        email_notifications_enabled: true
+      }
     });
 
     return updatedSettings;
